015e919833a1f183e443d48fea69b140560063d7
Костя <kroshavr@mail.ru>
2022-12-04 11:18:19 +0500

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задание 1
+        int [] numbers = new int [3];
+        numbers [0] = 1;
+        numbers [1] = 2;
+        numbers [2] = 3;
+    }
+}
